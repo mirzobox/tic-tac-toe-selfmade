eaaddcabb96fb78bba4ed3cb02c43175e2baee7c
mirzoulugbek <mirzoulugbekkhudoyberdiyev@gmail.com>
2023-12-01 20:32:07 +0500

    Set custom shadows to tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,16 @@ export default {
     fontFamily: {
       outfit: ["Outfit", "Arial", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      boxShadow: {
+        "dark-shadow": "0px -8px 0px 0px #10212a inset",
+        "dark-shadow-small": "0px -4px 0px 0px #10212a inset",
+        "light-yellow-shadow": "0px -8px 0px 0px #cc8b13 inset",
+        "light-yellow-shadow-small": "0px -4px 0px 0px #cc8b13 inset",
+        "silver-shadow": "0px -8px 0px 0px #118c87 inset",
+        "silver-shadow-small": "0px -4px 0px 0px #118c87 inset",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
