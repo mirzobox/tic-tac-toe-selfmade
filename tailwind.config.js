7ccfd5761ab6cd5640c85bf6508b409c8730bf9b
mirzoulugbek <mirzoulugbekkhudoyberdiyev@gmail.com>
2023-12-01 19:41:05 +0500

    Set custom colors to tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ["./**/*.{html,js}"],
   theme: {
+    colors: {
+      "light-blue": "#31c3bd",
+      "light-blue-hover": "#65e9e4",
+      "light-yellow": "#f2b137",
+      "light-yellow-hover": "#65e9e4",
+      "dark-navy": "#1a2a33",
+      "semi-dark-navy": "#1f3641",
+      silver: "#a8bfc9",
+      "silver-hover": "#dbe8ed",
+    },
     extend: {},
   },
   plugins: [],
