2f4cd433f5258c29943bbc0a077385f41a2a5319
mirzoulugbek <mirzoulugbekkhudoyberdiyev@gmail.com>
2023-12-02 11:44:01 +0500

    Style interface of game starter section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,12 @@ export default {
       "light-blue": "#31c3bd",
       "light-blue-hover": "#65e9e4",
       "light-yellow": "#f2b137",
-      "light-yellow-hover": "#65e9e4",
+      "light-yellow-hover": "#ffc860",
       "dark-navy": "#1a2a33",
       "semi-dark-navy": "#1f3641",
       silver: "#a8bfc9",
       silver_5: "rgba(168, 191, 201, 0.05)",
+      silver_50: "rgba(168, 191, 201, 0.5)",
       "silver-hover": "#dbe8ed",
     },
     fontFamily: {
