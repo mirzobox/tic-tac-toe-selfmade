2d0d5af885a7cd8d2957db22b92d3d73d2cac977
mirzoulugbek <mirzoulugbekkhudoyberdiyev@gmail.com>
2023-12-06 19:36:37 +0500

    Set (change attribute) choosen (x, o)

--- js/set-score-board.js
+++ js/set-score-board.js
@@ -1,19 +1,23 @@
-import { cpu, player, x } from "./elements.js";
+import { player, x } from "./elements.js";
 import { elOStatus, elXStatus } from "./html-elements.js";
 
 const setScoreBoard = (whichElIsUpdate) => {
+  const xData = elXStatus.dataset.x;
+  const oData = elXStatus.dataset.o;
   if (Object.keys(whichElIsUpdate).length === 0) {
-    elXStatus.innerText = `${elXStatus.dataset.x} (${elXStatus.dataset.cpu})`;
-    elOStatus.innerText = `${elOStatus.dataset.o} (${elOStatus.dataset.you})`;
+    elXStatus.innerText = `${xData} (${elXStatus.dataset.cpu})`;
+    elOStatus.innerText = `${oData} (${elOStatus.dataset.you})`;
   } else {
     if (whichElIsUpdate.with === player) {
-      elXStatus.innerText = `${elXStatus.dataset.x} (${elXStatus.dataset.p})`;
-      elOStatus.innerText = `${elOStatus.dataset.o} (${elOStatus.dataset.p})`;
+      elXStatus.innerText = `${xData} (${elXStatus.dataset.p})`;
+      elOStatus.innerText = `${oData} (${elOStatus.dataset.p})`;
     }
 
     if (whichElIsUpdate.playerPick === x && whichElIsUpdate.with !== player) {
-      elXStatus.innerText = `${elXStatus.dataset.x} (${elXStatus.dataset.you})`;
-      elOStatus.innerText = `${elOStatus.dataset.o} (${elOStatus.dataset.cpu})`;
+      elXStatus.innerText = `${xData} (${elXStatus.dataset.you})`;
+      elOStatus.innerText = `${oData} (${elOStatus.dataset.cpu})`;
+      elXStatus.setAttribute("id", elOStatus.getAttribute("id"));
+      elOStatus.setAttribute("id", elXStatus.getAttribute("id"));
     }
   }
 };
